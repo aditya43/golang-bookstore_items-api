29694e503df87c7c5134aa0164792ad559c932fd
Aditya Hajare <aditya43@gmail.com>
2021-05-11 19:32:40 +0530

    Create item endpoint

--- src/controllers/items_controller.go
+++ src/controllers/items_controller.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/aditya43/bookstore-oauth-go/oauth"
 	"github.com/aditya43/golang-bookstore_items-api/src/domain/items"
+	"github.com/aditya43/golang-bookstore_items-api/src/services"
 )
 
 var (
@@ -28,9 +29,21 @@ func (cont *itemsController) Create(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	_ = &items.Item{
+	item := &items.Item{
 		Seller: oauth.GetUserId(r),
 	}
+
+	result, err := services.ItemsService.Create(item)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(err.Status)
+		json.NewEncoder(w).Encode(err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(result)
 }
 
 func (cont *itemsController) Get(w http.ResponseWriter, r *http.Request) {
